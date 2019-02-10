388b2d1a90178c4e501f2a7d61bb099007c35eaf
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-10 13:13:04 -0500

    Create BaitItem component that passes all tests

--- src/components/baits/BaitItem.jsx
+++ src/components/baits/BaitItem.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import Link from 'react-router-dom/Link';
+import { number, string, bool, func } from 'prop-types';
 
-const BaitItem = props => (
-  <div />
+const BaitItem = ({
+  id,
+  name,
+  category,
+  image,
+  isInTackleBox,
+  addToTackleBox,
+}) => (
+  <div className="card">
+    <img
+      className="card-img-top bait"
+      src={image}
+      alt={name}
+    />
+
+    <div className="card-body">
+      <h5 className="card-title">{name}</h5>
+      <h6 className="card-subtitle">{category}</h6>
+
+      {isInTackleBox ? (
+        <Link to="/tackle-box" className="btn btn-secondary">
+          In Tackle Box
+        </Link>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={addToTackleBox(id)}
+        >
+          <i className="fas fa-toolbox" />
+          Tackle Box
+        </button>
+      )}
+    </div>
+  </div>
 );
+
+BaitItem.propTypes = {
+  id: number.isRequired,
+  name: string.isRequired,
+  category: string.isRequired,
+  image: string.isRequired,
+  isInTackleBox: bool.isRequired,
+  addToTackleBox: func.isRequired,
+};
 
 export default BaitItem;
--- src/components/baits/BaitItem.test.js
+++ src/components/baits/BaitItem.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
+import { HashRouter } from 'react-router-dom';
 import BaitItem from './BaitItem';
 
 describe('BaitItem', () => {
+  const addToTackleBox = jest.fn();
   const props = {
+    id: 1,
     name: 'Fat Rap',
     category: 'Crankbait',
     image: 'fat-rap.png',
+    isInTackleBox: false,
+    addToTackleBox,
   };
 
   it('renders witout crashing', () => {
@@ -29,6 +34,45 @@ describe('BaitItem', () => {
     it('displays the bait\'s image', () => {
       expect(wrapper.containsMatchingElement(
         <img src="fat-rap.png" alt="Fat Rap" />,
+      )).toBe(true);
+    });
+  });
+
+  describe('when not in tackle box', () => {
+    let wrapper;
+    beforeEach(() => { wrapper = shallow(<BaitItem {...props} />); });
+
+    it('should not be in tackle box', () => {
+      expect(wrapper.contains('In Tackle Box')).toBe(false);
+    });
+
+    it('displays a button to add to tackle box', () => {
+      expect(wrapper.find('button').text()).toBe('Tackle Box');
+    });
+
+    it('calls addToTackleBox with its id when user clicks Tackle Box button', () => {
+      wrapper.find('button').simulate('click');
+      expect(addToTackleBox).toBeCalledWith(1);
+    });
+  });
+
+  describe('when in tackle box', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(
+        <HashRouter>
+          <BaitItem {...props} isInTackleBox />
+        </HashRouter>,
+      );
+    });
+
+    it('should not contain button to add to tackle box', () => {
+      expect(wrapper.contains('Tackle Box')).toBe(false);
+    });
+
+    it('includes link to Tackle Box', () => {
+      expect(wrapper.containsMatchingElement(
+        <a href="#/tackle-box">In Tackle Box</a>,
       )).toBe(true);
     });
   });
