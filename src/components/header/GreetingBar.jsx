4ae1c525d15bdbcddd0d07eaa582f3b78f93020a
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-10 00:52:08 -0500

    Create GreetingBar component that passes all the tests

--- src/components/header/GreetingBar.jsx
+++ src/components/header/GreetingBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { string, bool, func } from 'prop-types';
 
 const GreetingBar = ({ username, isAuthenticated, logOut }) => (
   <nav className="navbar navbar-expand navbar-primary">
@@ -7,24 +8,35 @@ const GreetingBar = ({ username, isAuthenticated, logOut }) => (
       <i className="navbar-brand" />
 
       <ul className="navbar-nav">
-        <li className="nav-item">
-          <button type="button">Sign Out</button>
-        </li>
-        <li className="nav-item user">
-          <i className="far fa-user" />
-          {username}
-        </li>
-
-        <li className="nav-item">
-          <Link to="/login">Log In</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/signup">Sign Up</Link>
-        </li>
-
+        {isAuthenticated ? (
+          <>
+            <li className="nav-item">
+              <button type="button" onClick={logOut}>Sign Out</button>
+            </li>
+            <li className="nav-item user">
+              <i className="far fa-user" />
+              {username}
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link to="/login">Log In</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   </nav>
 );
+
+GreetingBar.propTypes = {
+  username: string.isRequired,
+  isAuthenticated: bool.isRequired,
+  logOut: func.isRequired,
+};
 
 export default GreetingBar;
