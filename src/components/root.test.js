440854f47bede544d0b2d3c57e67ee16d249ea5a
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-09 20:58:20 -0500

    Setup initial testing environment with enzyme. Write first smoke test for App and Root components

--- src/components/root.test.js
+++ src/components/root.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from 'redux-mock-store';
 
 import Root from './root';
 
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Root />, div);
+  const store = mockStore();
+
+  ReactDOM.render(<Root store={store} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
