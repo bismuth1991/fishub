a695644ad020a0b41f58b263fa1a354f5087d5aa
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-10 21:44:07 -0500

    Write test for EmptyTackleBox component

---
+++ src/components/tackle_box/EmptyTackleBox.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EmptyTackleBox = () => (<div />);
+
+export default EmptyTackleBox;
---
+++ src/components/tackle_box/EmptyTackleBox.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { HashRouter } from 'react-router-dom';
+
+import EmptyTackleBox from './EmptyTackleBox';
+
+describe('EmptyTackleBox', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<HashRouter><EmptyTackleBox /></HashRouter>);
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('displays a message to let user know it\'s empty', () => {
+    expect(wrapper.contains('An empty tackle box catches no fish.')).toBe(true);
+  });
+
+  it('includes link to /baits', () => {
+    expect(wrapper.containsMatchingElement(
+      <a href="#/baits">Fill&#39;er Up!</a>,
+    )).toBe(true);
+  });
+});
