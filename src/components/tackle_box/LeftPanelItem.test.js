da57100753e59b30bbd7a7fce7b9548d3993f079
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-10 22:12:00 -0500

    Write test for LeftPanelItem component

---
+++ src/components/tackle_box/LeftPanelItem.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import LeftPanelItem from './LeftPanelItem';
+
+describe('LeftPanelItem', () => {
+  const props = {
+    name: 'Fat Rap',
+    image: 'fat-rap.png',
+  };
+
+  it('renders without crashing', () => {
+    shallow(<LeftPanelItem {...props} />);
+  });
+
+  it('displays the bait\'s image', () => {
+    const wrapper = shallow(<LeftPanelItem {...props} />);
+
+    expect(wrapper.containsMatchingElement(
+      <img src={props.image} alt={props.name} />,
+    )).toBe(true);
+  });
+});
