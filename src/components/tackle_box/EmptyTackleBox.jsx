aa8384f7c23efff445d4ce976e879bf57b633016
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-10 21:49:10 -0500

    Create EmptyTackleBox component that passes all tests

--- src/components/tackle_box/EmptyTackleBox.jsx
+++ src/components/tackle_box/EmptyTackleBox.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const EmptyTackleBox = () => (<div />);
+const EmptyTackleBox = () => (
+  <div className="container empty-box">
+    <p>
+      <span>An empty tackle box catches no fish. </span>
+      <span role="img" aria-label="sad">😕</span>
+    </p>
+    <Link to="/baits">Fill&#39;er Up!</Link>
+  </div>
+);
 
 export default EmptyTackleBox;
