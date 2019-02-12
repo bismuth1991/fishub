ab2a0c0758e64ad1007564808503c2f2353339bb
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-12 13:14:05 -0800

    Create activitiesReducer that passes test

--- src/reducers/activitiesReducer.js
+++ src/reducers/activitiesReducer.js
@@ -1,7 +1,14 @@
+import { FETCH_ACTIVITIES_SUCCESS } from '../actions/activitiesActions';
+
 const initialState = {};
 
 const activitiesReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_ACTIVITIES_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+      };
     default:
       return state;
   }
