85e6c277468c1cab451b2231608be99aecb4e693
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-12 02:03:55 -0500

    Add fecthBaits thunk action

--- src/actions/baitsActions.js
+++ src/actions/baitsActions.js
@@ -1,6 +1,13 @@
+import API from '../utils/api_utils';
+
 export const FETCH_BAITS_SUCCESS = 'FETCH_BAITS_SUCCESS';
 
 export const fetchBaitsSuccess = payload => ({
   type: FETCH_BAITS_SUCCESS,
   payload,
 });
+
+export const fetchBaits = () => dispatch => (
+  API.fetchBaits()
+    .then(payload => dispatch(fetchBaitsSuccess(payload.data)))
+);
