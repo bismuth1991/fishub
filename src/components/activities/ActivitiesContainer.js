3e7457e8a9f3ecf4e89a7dc871dbc857054eb174
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-12 15:33:40 -0800

    Create ActivitiesContainer

--- src/components/activities/ActivitiesContainer.js
+++ src/components/activities/ActivitiesContainer.js
@@ -1,5 +1,19 @@
 import { connect } from 'react-redux';
 
+import { getActivities } from '../../selectors/activitiesSelectors';
 import { fetchActivities } from '../../actions/activitiesActions';
 
 import Activities from './Activities';
+
+const mapStateToProps = state => ({
+  activities: getActivities(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchActivities: () => dispatch(fetchActivities()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Activities);
